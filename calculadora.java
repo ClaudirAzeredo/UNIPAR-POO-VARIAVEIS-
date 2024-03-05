341ade2d9e858ed7577e1f5dd4e2b5158dca1010
ClaudirAzeredo <160553606+ClaudirAzeredo@users.noreply.github.com>
2024-03-04 22:17:18 -0300

    Create calculadora.java

---
+++ calculadora.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero:");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo numero:");
+        double numero2 = scanner.nextDouble();
+
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtracao: " + subtracao);
+        System.out.println("Multiplicacao: " + multiplicacao);
+        System.out.println("Divisao: " + divisao);
+
+        scanner.close();
+    }
+}
