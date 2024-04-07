e5a3a602ff547155b8accb892ef0d5ec45aed41a
ClaudirAzeredo <160553606+ClaudirAzeredo@users.noreply.github.com>
2024-04-07 20:18:21 -0300

    Create Atividade4.java

---
+++ Atividade4.java
@@ -0,0 +1,28 @@
+
+    public class ControleEstoque {
+    public static void main(String[] args) {
+       
+        String nomeProduto1, nomeProduto2, nomeProduto3; 
+        String categoriaProduto1, categoriaProduto2, categoriaProduto3;
+        double precoUnitarioProduto1, precoUnitarioProduto2, precoUnitarioProduto3; 
+        int quantidadeEstoqueProduto1, quantidadeEstoqueProduto2, quantidadeEstoqueProduto3;
+        
+      
+        nomeProduto1 = "Arroz";
+        categoriaProduto1 = "Alimentos";
+        precoUnitarioProduto1 = 10.5;
+        quantidadeEstoqueProduto1 = 100;
+        
+        nomeProduto2 = "Refrigerante";
+        categoriaProduto2 = "Bebidas";
+        precoUnitarioProduto2 = 5.0;
+        quantidadeEstoqueProduto2 = 50;
+        
+        nomeProduto3 = "Sabonete";
+        categoriaProduto3 = "Higiene";
+        precoUnitarioProduto3 = 2.0;
+        quantidadeEstoqueProduto3 = 200;
+        
+      
+    }
+}
