b46f1502c48cf80cc1ba8daa055ddb61841f9902
ClaudirAzeredo <160553606+ClaudirAzeredo@users.noreply.github.com>
2024-04-07 19:17:05 -0300

    Atividade2.java

--- Atividade2.java
+++ Atividade2.java
@@ -1,10 +1,12 @@
-nomeProduto
-      precoProduto
-      marcaDoProduto
-      quantidadeProduto
-      descricaoProduto
-        nomeProduto
-         numeroProduto
-         precoDoProduto
-         estoqueDisponivel
-         totalDeProdutos
+public class Produto {
+    private String nomeProduto;
+    private double precoProduto; 
+    private String marcaDoProduto; 
+    private int quantidadeProduto; 
+    private String descricaoProduto; 
+    private String nomeProduto; 
+    private int numeroProduto123; 
+    private double precoDoProduto; 
+    private int estoqueDisponivel; 
+    private int totalDeProdutos; 
+}
